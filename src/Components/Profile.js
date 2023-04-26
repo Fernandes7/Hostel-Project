3f44e14439cdb2804577ea6c145ea0e57c50ae67
Ferno <fernandeskj007@gmail.com>
2023-04-26 23:51:59 +0530

    imageset

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Profile.css"
 import { useLocation, useNavigate } from 'react-router'
 import axios from 'axios'
@@ -7,28 +7,46 @@ function Profile() {
   const history=useNavigate()
   const [selection,setSelecton]=useState(1)
   const [profileilmgenable,setProfileimageenable]=useState(false)
-  const [image,setimage]=useState({})
-  const [fetchedprofileimg,setfetchedprofileimg]=useState()
-  let base64String;
-  let binaryvalue;
+  const [data,setData]=useState({})
+  const [userimage,setUserimage]=useState()
+  let base64String
   const imagepopup=(value)=>{
     setProfileimageenable(value)
   }
-  const imagesave=(e)=>{
-    setimage({picture:e.target.files[0]})
+  const imagesave= async(e)=>{
+    const image=e.target.files[0]
+    const base64= await uploadimage(image)
+    setData({datas:base64})
   }
-  const uploadimage=()=>{
-    const formdata=new FormData()
-    formdata.append("image",image.picture)
-    axios.post(`http://localhost:8000/single/${profile.state.profile._id}`,formdata).then((responce)=>console.log(responce))
-    console.log("image is",image)
+  const uploadimage=(file)=>{
+  return new Promise((res,rej)=>{
+    const filereader=new FileReader()
+    filereader.readAsDataURL(file)
+    filereader.onload=()=>{
+      res(filereader.result)
+    }
+    filereader.onerror=(error)=>{
+      rej(error)
+    }
+  }) 
+  }
+  const upload=()=>{
+    axios.post(`http://localhost:8000/uploadimage/${profile.state.profile._id}`,{data}).then((responce)=>{
+      imagepopup(false)
+      alert("Image Updated Succesfully")
+      displayimagefunction()
+    })
   }
   const fetchimage=()=>{
     imagepopup(false)
-    axios.post(`http://localhost:8000/getimage/${profile.state.profile._id}`).then((responce)=>{
-    setfetchedprofileimg(responce.data[0].image.data.data)
-    fetchedprofileimg && console.log("herebaseeee",fetchedprofileimg)
-    fetchedprofileimg && (base64String = btoa(String.fromCharCode(...new Uint8Array(responce.data[0].image.data.data))))
+  }
+  useEffect(()=>{
+    displayimagefunction()
+  })
+  const displayimagefunction=()=>{
+      axios.get(`http://localhost:8000/getimage/${profile.state.profile._id}`).then((responce)=>{
+      base64String=responce.data.image
+      base64String && setUserimage(base64String)
     })
   }
   return (
@@ -36,10 +54,8 @@ function Profile() {
     <img className='profilebackimg' src="https://img.jamesedition.com/listing_images/2023/02/21/12/32/36/0236076f-4269-443b-acdb-2b82c87730de/je/1000x620xc.jpg" alt="images"></img>
     <div className='profiledetaildiv'>
     <div className='profileimagediv'>
-        {/* {fetchedprofileimg && fetchedprofileimg.map((value)=>{base64String = btoa(String.fromCharCode(...new Uint8Array(value.image.data.data)))
-        return <img src={`data:image/png;base64,${base64String}`} alt=""/>})} */}
-        <img src={`data:image/png;base64,${base64String}`} alt=""/>
-        <button className='detailbutton' onClick={()=>imagepopup(true)}>Add photo</button>
+        { <img src={userimage && userimage} alt=""/>}
+        <button className='detailbutton addimagebutton' onClick={()=>imagepopup(true)}>Add photo</button>
         <h3>Hey, {profile.state.profile.username}</h3>
     </div>
     <div className='profilecontent'>
@@ -97,7 +113,7 @@ function Profile() {
     </div>
     {profileilmgenable && <div className='profileupdatediv'>
       <input type="file" onChange={imagesave}></input>
-      <div> <button className='detailsbutton' onClick={fetchimage}>Close</button><button className='detailsbutton' onClick={uploadimage}>Upload</button></div>
+      <div> <button className='detailsbutton' onClick={fetchimage}>Close</button><button className='detailsbutton' onClick={upload}>Upload</button></div>
     </div>}
     </div>
   )
